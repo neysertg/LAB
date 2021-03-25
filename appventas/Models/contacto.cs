2d74da1364f6480dbfe6b138a94d8814851ee20e
Neyser Tacanga <tacangan@gmail.com>
2021-03-25 13:34:13 -0500

    SEMANA 2 DE LAB

---
+++ appventas/Models/contacto.cs
@@ -0,0 +1,9 @@
+using System;
+namespace appventas.Models{
+    public class contacto{ //UNA CLASE
+        public String FirstName{get; set;} //ATRIBUTOS
+        public String LastName{get; set;}
+        public String Email{get; set;}
+        public String Phone{get; set;}
+    }
+}
